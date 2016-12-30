b22b17b214ad21d8acf17428bf7684799b1d03d2
Jagdeep Singh <Jagdeep@druva.local>
2016-12-30 12:53:55 +0530

    initial commit for sample push notification test

---
+++ web/app/config.js
@@ -0,0 +1,4 @@
+window.GoogleSamples = window.GoogleSamples || {};
+window.GoogleSamples.Config = window.GoogleSamples.Config || {
+    gcmAPIKey: 'AAAAhCff1ec:APA91bF8wUT4rXl7LlNqF5w97OhinTUardgRpat32PCfr3d2K_ZedV1UuPNFeA2cX4cC6IM6M2gd6c-l52vriiE_DovVPeZ1hC5H3IkY_BTNyK0tL6JHNGvRsqP3tngh5n530lWDf9SbHl_RBYP06eprF2yPfvD1BQ'
+};
